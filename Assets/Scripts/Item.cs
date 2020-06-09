a7274c1c053d6ee9fac4ce62de643207738ab73b
0testarossa <lumina.fatetestarossa@gmail.com>
2020-06-09 15:48:33 +0200

    add map and player

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    private backpack backpackScript;
+    private float timer;
+    public bool canGet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        backpackScript = player.GetComponent<backpack>();
+        timer = 0;
+        canGet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 0.5)
+        {
+            canGet = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player" && canGet == true )
+        {   
+
+            backpackScript.putItem(itemName);
+            Destroy( gameObject );
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/backpack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class backpack : MonoBehaviour
+{
+
+    private int itemsAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemsAmount = 0;
+    }
+
+    public void putItem(string name)
+    {
+        string slotName = "Item (" + itemsAmount.ToString() + ")";
+        GameObject itemSlot = GameObject.Find(slotName);
+        itemSlot.GetComponent<Text>().text = name;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        
+    }
+}
