f7ac6fb6332b2f748c34d3b3fca33cd29199c100
0testarossa <lumina.fatetestarossa@gmail.com>
2020-06-09 18:00:02 +0200

    add npc animations

---
+++ Assets/Scripts/rabbitRunning.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rabbitRunning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1 * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime);
+        transform.Rotate(0, -150 * Time.deltaTime, 0);
+    }
+}
