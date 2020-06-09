a48abd6121a1ba1126ea69e2b37134b6892c3859
0testarossa <lumina.fatetestarossa@gmail.com>
2020-06-09 21:57:39 +0200

    add timer

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    private float time;
+    private float numberChangeTime;
+    private TextMeshProUGUI timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+        time = 300.0f;
+        numberChangeTime = 12.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time>0)
+        {
+            time -= Time.deltaTime;
+            if (numberChangeTime > 0)
+            {
+                numberChangeTime -= Time.deltaTime;
+            }
+            else
+            {
+                timerText.text = (Int16.Parse(timerText.text.Remove(timerText.text.Length - 1)) - 1).ToString() + "h";
+                numberChangeTime = 12.5f;
+            }
+        } else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
