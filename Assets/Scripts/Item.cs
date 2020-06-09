58085b6b88276ce171c08657e4fe32cf3976eed7
HaViXus <tukedarr@gmail.com>
2020-06-09 19:16:53 +0200

    zmiany Radka

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
@@ -8,13 +9,18 @@ public class Item : MonoBehaviour
     private backpack backpackScript;
     private float timer;
     public bool canGet;
+    public bool isPlayerNear;
+
+    GameObject message;
     // Start is called before the first frame update
     void Start()
     {
         GameObject player = GameObject.Find("Player");
+        message = GameObject.Find("Message");
         backpackScript = player.GetComponent<backpack>();
         timer = 0;
         canGet = false;
+        isPlayerNear = false;
     }
 
     // Update is called once per frame
@@ -25,17 +31,33 @@ public class Item : MonoBehaviour
         {
             canGet = true;
         }
+
+        if (isPlayerNear && Input.GetKey(KeyCode.F))
+        {
+            message.GetComponent<Text>().text = "";
+            backpackScript.putItem(itemName);
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player" && canGet == true )
-        {   
+        {
 
-            backpackScript.putItem(itemName);
-            Destroy( gameObject );
+            message.GetComponent<Text>().text = "Press [F] to take pick item";
+            isPlayerNear = true;
 
+        }
+    }
 
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.name == "Player" && canGet == true)
+        {
+            message.GetComponent<Text>().text = "";
+            isPlayerNear = false;
         }
     }
 }
--- Assets/Scripts/backpack.cs
+++ Assets/Scripts/backpack.cs
@@ -7,10 +7,12 @@ public class backpack : MonoBehaviour
 {
 
     private int itemsAmount;
+    private int placesAmount;
     // Start is called before the first frame update
     void Start()
     {
         itemsAmount = 0;
+        placesAmount = 0;
     }
 
     public void putItem(string name)
@@ -18,7 +20,22 @@ public class backpack : MonoBehaviour
         string slotName = "Item (" + itemsAmount.ToString() + ")";
         GameObject itemSlot = GameObject.Find(slotName);
         itemSlot.GetComponent<Text>().text = name;
-        
+        itemsAmount++;
+        GameObject backPack = GameObject.Find("BackpackView");
+        backPack.SetActive(false);
+        backPack.SetActive(true);
+    }
+
+    public void putPlace(string name)
+    {
+        string slotName = "Place (" + placesAmount.ToString() + ")";
+        GameObject itemSlot = GameObject.Find(slotName);
+        print(itemSlot + " " + slotName);
+        itemSlot.GetComponent<Text>().text = name;
+        placesAmount++;
+        GameObject backPack = GameObject.Find("BackpackView");
+        backPack.SetActive(false);
+        backPack.SetActive(true);
     }
 
     // Update is called once per frame
